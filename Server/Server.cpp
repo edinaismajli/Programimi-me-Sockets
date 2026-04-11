8460000065ec67120af78e349086a23184135988
edinaismajli <edina.ismajli@student.uni-pr.edu>
2026-04-11 16:40:23 -0100

    Start client communication thread

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -3,4 +3,14 @@ SOCKET incoming = accept(server_socket, NULL, NULL);
 if (incoming != INVALID_SOCKET)
 {
     client[temp_id] = { temp_id, incoming };
+    
+    std::string id = std::to_string(temp_id);
+    send(incoming, id.c_str(), id.size(), 0);
+
+    my_thread[temp_id] = std::thread(
+        process_client,
+        std::ref(client[temp_id]),
+        std::ref(client),
+        std::ref(my_thread[temp_id])
+    );
 }
