937546e2c6fa0a8eac1e4e5df1f515cc42e15a0a
edinaismajli <edina.ismajli@student.uni-pr.edu>
2026-04-17 22:56:18 -0100

    Initialize Winsock library

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -1,16 +1,22 @@
-SOCKET incoming = accept(server_socket, NULL, NULL);
-
-if (incoming != INVALID_SOCKET)
+int main()
 {
-    client[temp_id] = { temp_id, incoming };
-    
-    std::string id = std::to_string(temp_id);
-    send(incoming, id.c_str(), id.size(), 0);
+    WSADATA wsaData;
+    WSAStartup(MAKEWORD(2, 2), &wsaData);
 
-    my_thread[temp_id] = std::thread(
-        process_client,
-        std::ref(client[temp_id]),
-        std::ref(client),
-        std::ref(my_thread[temp_id])
-    );
+    SOCKET incoming = accept(server_socket, NULL, NULL);
+
+    if (incoming != INVALID_SOCKET)
+    {
+        client[temp_id] = { temp_id, incoming };
+        
+        std::string id = std::to_string(temp_id);
+        send(incoming, id.c_str(), id.size(), 0);
+
+        my_thread[temp_id] = std::thread(
+            process_client,
+            std::ref(client[temp_id]),
+            std::ref(client),
+            std::ref(my_thread[temp_id])
+        );
+    }
 }
